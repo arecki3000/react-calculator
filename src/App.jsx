27a1cb5651a01b5cc953c8132534a72fde7d5020
Dominik Krzywiecki <dominik@krzywiecki.com>
2018-03-18 11:48:10 +0100

    Add fresh react project with basic setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+  render() {
+    return <div className="App">Calculator component will be here :)</div>;
+  }
+}
+
+export default App;
